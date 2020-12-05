2fb41cb90f5dd28dd979aec223c34cc1527f3c39
James Wall <james.wall95@gmail.com>
2020-12-05 14:53:08 -0800

    got webcam hooked up to client.py

--- client/client.py
+++ client/client.py
@@ -38,37 +38,30 @@ def parse_arguments():
 
 if __name__ == "__main__":
     args = parse_arguments()
-
     print("Starting video stream...")
     video_capture = cv2.VideoCapture(0)
 
-    """
-    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-
-    while True:
-        ret, frame = video_capture.read()
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(gray, scaleFactor = 1.1, minNeighbors = 8)
-
-        for (x, y, w, h) in faces:
-            gray = gray[y:y+h, x:x+w]
-            cv2.rectangle(gray, (x, y), (x+w, y+h), (0, 255, 0), 2)
-            cur_time = datetime.now().strftime("%m_%d_%Y-%H_%M_%S")
-            out_file = f'{cur_time}.png'
-            #cv2.imwrite(out_file, gray)
-            #print(f'Face found! Written to {out_file}')
-            
-            encoded_img = cv2.imencode('.png', gray)[1].tobytes()
-            publish.single(args.MQTT_TOPIC, payload=encoded_img, hostname=args.MQTT_SERVER, port=args.MQTT_PORT, qos=args.MQTT_QOS)
-            print(f'Published {out_file} to {args.MQTT_SERVER}:{args.MQTT_PORT} topic: {args.MQTT_TOPIC}')
-            sleep(5)
-
-        #cv2.imshow('Video', frame)
-        #if cv2.waitKey(1) & 0xFF == ord('q'):
-        #    break
-    """
 
     #TODO: Add video capture and publishing here
+
+    while(True):
+        # Capture frame-by-frame.
+        ret, frame = video_capture.read()
+
+        # Throw out the color information and get the gray frame.
+        # TODO maybe have in color?
+        # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        print("here1: " + str(ret))
+        if ret == True:
+            # Display the resulting frame
+            cv2.imshow('Frame', frame)
+            print("here2")
+
+        # Break the loop
+        else:
+            break
+
+
 
     video_capture.release()
     cv2.destroyAllWindows()

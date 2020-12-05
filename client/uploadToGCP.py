268e98242f1f43b1d3d1736f7a2aefc88af70918
James Wall <james.wall95@gmail.com>
2020-12-05 15:49:38 -0800

    added commands for reference

--- client/uploadToGCP.py
+++ client/uploadToGCP.py
@@ -1,1 +1,20 @@
+from gcloud import storage
+from oauth2client.service_account import ServiceAccountCredentials
+import os
+
+credentials_dict = {
+    'type': 'service_account',
+    'client_id': os.environ['BACKUP_CLIENT_ID'],
+    'client_email': os.environ['BACKUP_CLIENT_EMAIL'],
+    'private_key_id': os.environ['BACKUP_PRIVATE_KEY_ID'],
+    'private_key': os.environ['BACKUP_PRIVATE_KEY'],
+}
+credentials = ServiceAccountCredentials.from_json_keyfile_dict(
+    credentials_dict
+)
+client = storage.Client(credentials=credentials, project='myproject')
+bucket = client.get_bucket('mybucket')
+blob = bucket.blob('myfile')
+blob.upload_from_filename('myfile')
+
 print("test output for GCP py file")
